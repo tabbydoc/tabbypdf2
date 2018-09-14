49af6ebd15a8bb2ce05f36ec51b1f97e5d7c2d2c
ElliRoy <31957585+ElliRoy@users.noreply.github.com>
2018-09-14 20:26:08 +0800

    Add files via upload
    Add block composition

---
+++ PageComposer.java
@@ -0,0 +1,16 @@
+package ru.icc.td.tabbypdf2.comp;
+
+import ru.icc.td.tabbypdf2.model.Page;
+
+public class PageComposer {
+    private final WordComposer wordComposer = new WordComposer();
+    private final BlockComposer blockComposer = new BlockComposer();
+
+    public void compose(Page page) {
+        if (null == page)
+            return;
+
+        wordComposer.composeWords(page);
+        blockComposer.composeBlocks(page);
+    }
+}
