8417c00f45f1cbb41c31345e7c2c7f718564c64f
ElliRoy <rozhcow1@gmail.com>
2019-04-06 17:35:21 +0800

    Add structure verification

--- src/main/java/ru/icc/td/tabbypdf2/detect/processing/verification/StructureVerification.java
+++ src/main/java/ru/icc/td/tabbypdf2/detect/processing/verification/StructureVerification.java
@@ -1,11 +1,22 @@
 package ru.icc.td.tabbypdf2.detect.processing.verification;
 
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import ru.icc.td.tabbypdf2.model.Block;
 import ru.icc.td.tabbypdf2.model.Prediction;
+
+import java.util.Set;
 
 public class StructureVerification implements Verification {
 
     @Override
     public boolean verify(Prediction prediction) {
-        return prediction.getStructure() != null;
+        Graph<Block, DefaultWeightedEdge> graph = prediction.getStructure();
+        Set<Block> vertexes = graph.vertexSet();
+        Set<DefaultWeightedEdge> edges = graph.edgeSet();
+
+        return ((prediction.getStructure() != null) &&
+                !vertexes.isEmpty() &&
+                !edges.isEmpty());
     }
 }
