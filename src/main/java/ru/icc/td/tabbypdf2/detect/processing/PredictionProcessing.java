1a7b21530059f3b626a6494fdca9f0bb6d4ee1c4
ElliRoy <rozhcow1@gmail.com>
2019-04-10 13:04:57 +0800

    Add diagram verification

--- src/main/java/ru/icc/td/tabbypdf2/detect/processing/PredictionProcessing.java
+++ src/main/java/ru/icc/td/tabbypdf2/detect/processing/PredictionProcessing.java
@@ -3,6 +3,7 @@ package ru.icc.td.tabbypdf2.detect.processing;
 import ru.icc.td.tabbypdf2.detect.processing.recognition.StructureRecognizer;
 import ru.icc.td.tabbypdf2.detect.processing.refinement.ParagraphRefinement;
 import ru.icc.td.tabbypdf2.detect.processing.refinement.Refinement;
+import ru.icc.td.tabbypdf2.detect.processing.verification.DiagramVerification;
 import ru.icc.td.tabbypdf2.detect.processing.verification.ImageVerification;
 import ru.icc.td.tabbypdf2.detect.processing.verification.StructureVerification;
 import ru.icc.td.tabbypdf2.detect.processing.verification.Verification;
@@ -54,6 +55,7 @@ public class PredictionProcessing {
         // Verifications
         verifications.add(new StructureVerification());
         verifications.add(new ImageVerification());
+        verifications.add(new DiagramVerification());
         // verifications.add(new ParagraphVerification());
         // Refinements
         refinements.add(new ParagraphRefinement());
---
+++ src/main/java/ru/icc/td/tabbypdf2/detect/processing/verification/DiagramVerification.java
@@ -0,0 +1,22 @@
+package ru.icc.td.tabbypdf2.detect.processing.verification;
+
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import ru.icc.td.tabbypdf2.model.Block;
+import ru.icc.td.tabbypdf2.model.Prediction;
+
+public class DiagramVerification implements Verification {
+
+    @Override
+    public boolean verify(Prediction prediction) {
+        Graph<Block, DefaultWeightedEdge> structure = prediction.getStructure();
+        DescriptiveStatistics ds = new DescriptiveStatistics();
+
+        for (DefaultWeightedEdge edge : structure.edgeSet()) {
+            ds.addValue(structure.getEdgeWeight(edge));
+        }
+        System.out.printf("Mean %f, Page %d\n", ds.getMean(), prediction.getPage().getIndex());
+        return ds.getMean() <= 1.5f && ds.getMean() >= 1f;
+    }
+}
